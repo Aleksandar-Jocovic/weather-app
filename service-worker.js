36c1e381b09b8a88a11750a6fe52556e90127d6f
aleksandar <jocovic099@gmail.com>
2020-05-15 00:24:13 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather-app/precache-manifest.4210b8b493290d21e42cd8fafd400641.js"
+  "/weather-app/precache-manifest.987edbf893eeecc6373d35e90b2b981f.js"
 );
 
 self.addEventListener('message', (event) => {
